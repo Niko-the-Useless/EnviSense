55aeec7aa1183f21b2fd153e71784b0458470fbf
Niko <radosz2003@gazeta.pl>
2025-04-06 16:35:50 +0200

    fixed stuff

--- enviSense.asm
+++ enviSense.asm
@@ -6,3 +6,14 @@ setup:
 	out DDRC,r16;DDRC output
 	ldi r16,0b00000000
 	out DDRD,r16;DDRD input
+main:
+	lcdSetting lcdOn
+	lcdSetting lcdClear 
+	lcdSetting lcdFunction
+	lcdSetting lcdEntry
+	lcdWrite SN
+	lcdWrite SI
+	lcdWrite SK
+	lcdWrite SO
+loop:
+	rjmp loop
