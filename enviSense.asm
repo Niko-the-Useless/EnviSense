a18ba2c8f8136d5be48e451d05ab17514db0306f
Niko <radosz2003@gazeta.pl>
2025-04-08 15:27:59 +0200

    file formatting + spi receive

--- enviSense.asm
+++ enviSense.asm
@@ -1,16 +1,27 @@
 .include "defs.inc"
-.include "macros.inc"
+.include "lib.inc"
 setup:
+;pin_setup
 	ldi r16,0b11111111
-	out DDRA,r16;DDRA output
-	out DDRC,r16;DDRC output
+	out DDRA,r16;DDRA output LCD
+	out DDRC,r16;DDRC output LCD
 	ldi r16,0b00000000
-	out DDRD,r16;DDRD input
-main:
+	out DDRD,r16;DDRD input BUTTONS + 1wire
+	ldi r16,0b00001101
+	out DDRB,r16;DDRB SS out, mosi out, miso in, sck out
+;lcd_setup
 	lcdSetting lcdOn
 	lcdSetting lcdClear 
 	lcdSetting lcdFunction
 	lcdSetting lcdEntry
+;spi_setup
+	sbi PORTB, 4
+	ldi r16, 0b01010000
+	out SPCR, r16
+;i2c_setup
+;wire_setup
+;interrupt_setup
+main:
 	lcdWrite SN
 	lcdWrite SI
 	lcdWrite SK
